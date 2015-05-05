490332ce05b1c1fe51bf5e82ed95456095575651
Archie <yangyxcn@gmail.com>
2015-05-05 08:22:53 +0800

    Add event detail view controller
    Signed-off-by: Archie <yangyxcn@gmail.com>

---
+++ CityLive/EventDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EventDetailViewController.swift
+//  CityLive
+//
+//  Created by archie on 15/5/5.
+//  Copyright (c) 2015年 archie. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hidesBottomBarWhenPushed = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
